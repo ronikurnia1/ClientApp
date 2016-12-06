8e61868381f710e32962860cd76ac8f1eaacc89f
Roni Kurniawan <roni.kurniawan@corp.dipostar.com>
2016-12-06 11:39:22 +0700

    add login form

--- src/client/app/login/login.component.ts
+++ src/client/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare const fabric: any;
 
 /**
  * This class represents the AdminComponent.
@@ -9,6 +12,28 @@ import { Component } from '@angular/core';
   templateUrl: 'login.component.html',
   styleUrls: ['login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
+
+  constructor(private router: Router, private elementRef: ElementRef) {
+
+  }
+
+  login(event: any) {
+    this.router.navigate(["home"]);
+  }
+
+  ngAfterViewInit() {
+    let TextFieldElements = this.elementRef.nativeElement.querySelectorAll(".ms-TextField");
+    for (var i = 0; i < TextFieldElements.length; i++) {
+      new fabric['TextField'](TextFieldElements[i]);
+    }
+
+    let ButtonElements = this.elementRef.nativeElement.querySelectorAll(".ms-Button");
+    for (var i = 0; i < ButtonElements.length; i++) {
+      new fabric['Button'](ButtonElements[i], function () {
+        // Insert Event Here
+      });
+    }
+  }
 
 }
