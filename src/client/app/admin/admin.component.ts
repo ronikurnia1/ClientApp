042641b6080100f96818f312612b7964df3b726b
ronikurnia1 <roni.kurniawan@outlook.com>
2017-04-16 23:07:45 +0700

    reactive form module

--- src/client/app/admin/admin.component.ts
+++ src/client/app/admin/admin.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Field } from '../shared/dynamic-form/models/field.interface';
+import { DynamicFormComponent } from '../shared/dynamic-form/dynamic-form.component';
+import { Validators } from '@angular/forms';
 
 /**
  * This class represents the AdminComponent.
@@ -9,11 +12,70 @@ import { Component } from '@angular/core';
   templateUrl: 'admin.component.html',
   styleUrls: ['admin.component.css']
 })
-export class AdminComponent {
+export class AdminComponent implements AfterViewInit {
+  @ViewChild(DynamicFormComponent) form: DynamicFormComponent;
 
-  displayConfig(event: any) {
-    let config = JSON.parse(localStorage.getItem("appConfig")) || {};
-    let sections: any[] = config.navigations;
-    sections.forEach(itm => alert(itm.displayName));
+  fields: Field[] = [
+    {
+      key: 'fullName',
+      type: 'input',
+      label: 'Full name',
+      name: 'name',
+      order: 0,
+      value: 'Roni Kurniawan',
+      placeholder: 'Enter your name',
+      validation: [Validators.required, Validators.minLength(4)]
+    },
+    {
+      key: 'datepicker',
+      type: 'datepicker',
+      label: 'Date of birth',
+      name: 'datepicker',
+      order: 1,
+      placeholder: 'Date of birth',
+      validation: [Validators.required]
+    },
+    {
+      key: 'dropdown',
+      order: 2,
+      type: 'dropdown',
+      label: 'Favourite Food',
+      name: 'food',
+      options: [{ key: 'key0', value: 'Pizza', selected: false },
+      { key: 'key1', value: 'Cofee', selected: false },
+      { key: 'key2', value: 'Pasta', selected: true },
+      { key: 'key3', value: 'Noddle', selected: false }],
+      placeholder: 'Select an option',
+      validation: [Validators.required]
+    },
+    {
+      key: 'submitButton',
+      order: 5,
+      label: 'Submit',
+      name: 'submit',
+      type: 'button'
+    }
+
+  ];
+
+  ngAfterViewInit() {
+    let previousValid = this.form.valid;
+    this.form.changes.subscribe(() => {
+      if (this.form.valid !== previousValid) {
+        previousValid = this.form.valid;
+        this.form.setDisabled('submit', !previousValid);
+      }
+    });
+
+    this.form.setDisabled('submit', true);
+    //this.form.setValue('name', 'Todd Motto');
   }
+
+  submit(value: { [name: string]: any }) {
+    console.log(JSON.stringify(value));
+  }
+
+
+
+
 }
--- src/client/app/admin/admin.module.ts
+++ src/client/app/admin/admin.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { AdminComponent } from './admin.component';
 import { AdminRoutingModule } from './admin-routing.module';
+import { DynamicFormModule } from "../shared/dynamic-form/dynamic-form.module";
 
 @NgModule({
-  imports: [AdminRoutingModule],
+  imports: [AdminRoutingModule, DynamicFormModule],
   declarations: [AdminComponent],
   exports: [AdminComponent]
 })
