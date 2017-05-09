126817bc0029401d2f41ba3dbdd9f90d199ee308
ronikurnia1 <roni.kurniawan@outlook.com>
2017-05-10 01:53:38 +0700

    fixing bug

--- src/client/app/access-group/details/threeview-checkbox/threeview-checkbox.component.ts
+++ src/client/app/access-group/details/threeview-checkbox/threeview-checkbox.component.ts
@@ -19,6 +19,8 @@ export class ThreeviewCheckboxComponent implements OnInit, AfterViewInit, AfterC
   constructor() { }
 
   public toggle(): void {
+    event.stopPropagation();
+    event.preventDefault();
     this.accessRole.isCollapsed = !this.accessRole.isCollapsed;
   }
 
@@ -27,7 +29,22 @@ export class ThreeviewCheckboxComponent implements OnInit, AfterViewInit, AfterC
 
   ngAfterViewInit() {
     new fabric['CheckBox'](this.checkBoxElement.nativeElement);
+
+    // Bug: not working at first click
+    // Bug workaround: need to click to make it working at
+    // the first click
+    let clickEvent = new MouseEvent("click", {
+      "view": window, bubbles: true,
+      cancelable: false
+    });
+    let element = this.checkBoxElement.nativeElement.querySelector(".ms-CheckBox-field");
+    element.dispatchEvent(clickEvent);
+    // collapse it
+    this.accessRole.isCollapsed = true;
+    element = this.checkBoxElement.nativeElement.querySelector(".ms-Label");
+    element.dispatchEvent(clickEvent);
   }
+
 
   ngAfterContentInit() {
   }
